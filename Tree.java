c3491d8dd0f543bf160ec35429eb652f45502148
Cody Noel <code.codynoel@gmail.com>
2019-12-10 14:33:16 -0700

    Create Tree.java
    quick practice with prints in java

---
+++ Tree.java
@@ -0,0 +1,14 @@
+public class Tree {
+    public static void main(String[] args) {
+        // Print a tree when introductions happen
+        System.out.println("Hey there, I'm Cody and i'm learning to code in Java!");
+        System.out.println("Ready to get my hands dirty!");
+        System.out.println("    {}");
+        System.out.println("    **");
+        System.out.println("   ****");
+        System.out.println("  ******");
+        System.out.println(" ********");
+        System.out.println("**********");
+        System.out.println("    []");
+    }
+}
